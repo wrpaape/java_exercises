187be16012f2b01089dd773b1406bc40b9aa9fe4
Reid Paape <wrpaape@gmail.com>
2015-12-24 09:53:06 -0600

    with constructor

--- Account.java
+++ Account.java
@@ -2,4 +2,28 @@ class Account {
   int accountNo;
   String accountName;
   double balance;
+
+  public Account(int no, String name, double bal) {
+    accountNo = no;
+    accountName = name;
+    balance = bal;
+  }
+
+  public void deposit(double amount) {
+    balance += amount;
+  }
+
+  public void clearBalance() {
+    balance = 0;
+  }
+
+  public double withdraw(double amount) {
+    if (balance - amount < 0) {
+      System.out.println("Insufficient Funds");
+    } else {
+      balance -= amount;
+    }
+
+    return balance;
+  }
 }
--- CreateAccount.java
+++ CreateAccount.java
@@ -1,10 +1,6 @@
 class CreateAccount {
   public static void main(String[] args) {
-    Account reidsAccount = new Account();
-
-    reidsAccount.accountNo = 123;
-    reidsAccount.accountName = "Reid";
-    reidsAccount.balance = 50;
+    Account reidsAccount = new Account(123, "Reid", 50);
   
     System.out.println(
       "A/c no: " + reidsAccount.accountNo +

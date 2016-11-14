21310cfe6275a24a60150e521ef974c5d5d007c0
Reid Paape <wrpaape@gmail.com>
2016-11-13 18:11:45 -0600

    more network

---
+++ brackets/Brackets.java
@@ -0,0 +1,18 @@
+public static class Brackets {
+	public static void main(String[] args) {
+
+
+	}
+
+	private static class StringIterator {
+		private String string;
+		private int index;
+
+		public StringIterator(String string) {
+			this.string = string;
+			this.index  = 0;
+		}
+
+
+	}
+}
